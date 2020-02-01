59faba067070bc04ef54d387177db7b56c9ca016
scouflaire <thomas.ghislain26@gmail.com>
2020-02-02 00:20:53 +0100

    nice

--- Assets/script/control_character.cs
+++ Assets/script/control_character.cs
@@ -9,7 +9,6 @@ public class control_character : MonoBehaviour
     private int speed = 10;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -19,14 +18,15 @@ public class control_character : MonoBehaviour
         float yrotate = transform.localEulerAngles.y;
         transform.eulerAngles = new Vector3(0, Mathf.Atan2(0, 0) * Mathf.Rad2Deg, 0);*/
 
-        transform.position -= (Vector3.forward * Input.GetAxis("Horizontal") * 10 * Time.deltaTime);
-        transform.position += (Vector3.right * Input.GetAxis("Vertical") * 10 * Time.deltaTime);
-        //transform.position += hinput.gamepad[1].leftStick.worldPositionFlat * speed * Time.deltaTime;
+        //transform.position -= (Vector3.forward * Input.GetAxis("Horizontal") * 10 * Time.deltaTime);
+        //transform.position += (Vector3.right * Input.GetAxis("Vertical") * 10 * Time.deltaTime);
+        transform.position += hinput.gamepad[1].leftStick.worldPositionFlat * speed * Time.deltaTime;
 
         //transform.eulerAngles = new Vector3(xrotate, Mathf.Atan2(xrotate, yrotate) * Mathf.Rad2Deg, 0);
 
-        Vector3 direction = new Vector3(Input.GetAxis("RVertical"), 0, Input.GetAxis("RHorizontal"));
+        //Vector3 direction = new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
         //Vector3 direction = new Vector3(Input.GetAxis("RHorizontal"), 0, Input.GetAxis("RVertical"));
+        Vector3 direction = hinput.gamepad[1].rightStick.worldPositionFlat;
         /*if (x != 0f || y != 0f)
         {
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(x, y) * Mathf.Rad2Deg, transform.eulerAngles.z);
