0c2b478c8b48ed0d447c45e1bff02adea42544df
4rksha <zacharia.beddalia@gmail.com>
2020-02-02 05:16:56 +0100

    Commit Zac

---
+++ Assets/Scripts/BossAI.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossAI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Vector3 currentDestination;
+    public Transform target;
+    Animator animator;
+
+    private bool isAttacking = false;
+    private bool isMoving = false;
+
+    private float cooldown = 2f;
+
+    void Start()
+    {
+        animator = transform.GetChild(0).GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        isAttacking = animator.GetBool("isAttacking");
+        isMoving = animator.GetBool("isMoving");
+        if (isMoving) 
+        {
+            MoveToDestination(); 
+        }
+        else if (isAttacking)
+        {
+
+        }
+        else if (cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
+        else
+        {
+            initiateNextMove();
+        }
+
+
+    }
+
+    void MoveToDestination()
+    {
+        agent.destination = currentDestination;
+        if (Vector3.Distance(transform.position, currentDestination) <= 5.2)
+        {
+            animator.SetBool("isMoving", false);
+            animator.SetBool("isAttacking", true);
+        }
+    }
+
+    void initiateNextMove() {
+            cooldown = 3f;
+            currentDestination = target.position;
+            transform.LookAt(new Vector3(currentDestination.x, transform.position.y, currentDestination.z));
+            if (Vector3.Distance(transform.position, currentDestination) <= 5.2)
+            {
+                isAttacking = true;
+                animator.SetBool("isAttacking", true);
+            }
+            else
+            {
+                isMoving = true;
+                animator.SetBool("isMoving", true);
+            }
+    }
+}
---
+++ Assets/ShaderScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderScript : MonoBehaviour
+{
+    Renderer rend;
+
+    public float minimum = 0;
+    public float maximum = 1.0F;
+
+    static float t = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float f = Mathf.Lerp(minimum, maximum, t);
+        t += 1f * Time.deltaTime;
+        if (t > 1.0f)
+        {
+            float temp = maximum;
+            maximum = minimum;
+            minimum = temp;
+            t = 0.0f;
+        }
+        Debug.Log(rend.material.GetColor("_Color").ToString());
+        rend.material.SetColor("_Color", new Color(1f, f, f));
+    }
+}
